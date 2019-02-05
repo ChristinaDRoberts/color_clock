b3fbbbcd7a6c3166487fed3013a4cb3359fc69be
Christina <Christinepeninsula@hotmail.com>
2019-02-05 08:59:23 -0500

    hexcolor update

--- color_clock/static/main.js
+++ color_clock/static/main.js
@@ -12,7 +12,7 @@ function displayTime() {
     if (minute <= 9) minute = '0' + minute;
     if (second <= 9) second = '0' + second;
 
-    console.log(second);
+    // console.log(second);
 
     document.querySelector('.time')
         .textContent = hour + ":" + minute + ":" + second;
@@ -22,17 +22,21 @@ function displayTime() {
 
     result = x * per;
     percent = result + "%";
-    console.log(percent);
+    // console.log(percent);
 
 
-    var hexHour = 0 + hour.toString(16).slice(-2);
-    var hexMinute = 0 + minute.toString(16).slice(-2);
-    var hexSecond = 0 + second.toString(16).slice(-2);
+    var hexHour = ('0' + hour.toString(16)).slice(-2);
+    var hexMinute = ('0' + minute.toString(16)).slice(-2);
+    var hexSecond = ('0' + second.toString(16)).slice(-2);
 
-    color = (hexHour + hexMinute + hexSecond);
+    var color = "#" + hexSecond + hexMinute + hexSecond;
+
+    console.log('color', color);
 
 
-    document.getElementById("timeid").style.background = color;
+
+
+    document.getElementById("timeid").style.backgroundColor = color;
 }
 
 var timer = setInterval(displayTime, 1000);
